2401f922fb50b735f82446da27551a50ad62b80a
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2022-02-09 12:36:28 -0800

    feat: add temperture conversion perl script

---
+++ ch2/convert.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# use strict;
+# use warnings FATAL => 'all';
+print "Enter a temperature (e.g., 32F, 100C):\n";
+$input = <STDIN>;
+chomp($input);
+
+if($input =~ m/^([-+]?[0-9]+)([CF])$/)
+{
+    $InputNum = $1;
+    $type     = $2;
+
+    if($type eq "C"){
+        $celsius = $InputNum;
+        $fahrenheit = ($celsius * 9 / 5) + 32;
+    } else {
+
+        $fahrenheit = $InputNum;
+        $celsius = ($fahrenheit -32) * 5 / 9;
+    }
+    printf "% .2f C is % .2f F/n", $celsius, $fahrenheit;
+} else {
+    printf "Expecting a number followed by \"C\" or \"F\",\n";
+    printf "so I don't understand \"$input\".\n";
+}
